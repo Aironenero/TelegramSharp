93324038e5803e955721ba300a648cc88f4051d9
Nickframe76 <nick.frame.76@gmail.com>
2016-08-05 18:45:46 +0200

    Fixed KeyboardBuilder, added TextBuilder and more events to play with!

--- Core/Objects/NetAPI/Keyboard/IReplyMarkup.cs
+++ Core/Objects/NetAPI/Keyboard/IReplyMarkup.cs
@@ -9,6 +9,10 @@ namespace TelegramSharp.Core.Objects.NetAPI.Keyboard
     public interface IReplyMarkup
     {
 
+        /// <summary>
+        /// Returns a serialized version of the class.
+        /// </summary>
+        /// <returns></returns>
         string serialize();
 
     }
---
+++ Core/Objects/NetAPI/TextBuilder/BaseComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramSharp.Core.Objects.NetAPI.TextBuilder
+{
+    public interface IBaseComponent
+    {
+        /// <summary>
+        /// Makes the message with the markup. If no markup is applied, it will return a string with no markup.
+        /// </summary>
+        /// <returns>Returns the string with markup.</returns>
+        string Make();
+
+        /// <summary>
+        /// Gets the ParsingMode used to parse the message.
+        /// </summary>
+        /// <returns></returns>
+        ParsingMode GetParsingMode();
+        
+
+    }
+}
