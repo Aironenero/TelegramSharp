4a4dc66d8be2f18b3275c6735327cd4f6d624291
Fabio <dsfabiospam@gmail.com>
2016-10-06 14:17:32 +0200

    license updated and added where missing

--- Core/MessageParser.cs
+++ Core/MessageParser.cs
@@ -1,5 +1,5 @@
 //TelegramSharp - A library to make telegram bots
-//Copyright (C) 2016  Samuele Lorefice, Niccol� Mattei
+//Copyright (C) 2016  Samuele Lorefice, Niccolò Mattei, Fabio De Simone
 //
 //This program is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
